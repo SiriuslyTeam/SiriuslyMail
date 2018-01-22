6c49d4413ebe1565ca69dafc975cf4c28e26348f
grafo <grafoffsergej@yandex.ru>
2018-01-22 21:16:40 +0300

    добавлена проверка отправки письма

--- app/src/main/java/edu/sirius/android/siriuslymail/SendActivity.java
+++ app/src/main/java/edu/sirius/android/siriuslymail/SendActivity.java
@@ -1,5 +1,8 @@
 package edu.sirius.android.siriuslymail;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,10 +10,15 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import static edu.sirius.android.siriuslymail.PostService.SUCCESS_LOGIN;
+import static edu.sirius.android.siriuslymail.PostService.SUCCESS_SEND;
 
 public class SendActivity extends AppCompatActivity {
 
     final String TAG = "lifecycle_send";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +36,18 @@ public class SendActivity extends AppCompatActivity {
                 finish();
             }
         });
+        BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                boolean success = intent.getExtras().getBoolean(SUCCESS_SEND);
+                if (success) {
+                    Toast toast = Toast.makeText(getApplicationContext(),"Message was successfuly sent",Toast.LENGTH_LONG);
+                } else {
+                    Toast toast = Toast.makeText(getApplicationContext(), "", Toast.LENGTH_LONG);
+                    toast.show();
+                }
+            }
+        };
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
